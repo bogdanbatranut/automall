2b70fd6942091ba2a96219ecc3434fcb2ca1ef37
Bogdan Batranut <bogdan.batranut@plentymarkets.com>
2024-01-09 14:28:20 +0200

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {FilterComponent} from "./ads/filter/filter.component";
+import {MainBarComponent} from "./main-bar/main-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FilterComponent],
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FilterComponent, MainBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {AdsListComponent} from "./ads/ads-list/ads-list.component";
 import {LearnComponent} from "./learn/learn/learn.component";
+import {AdsFilterComponent} from "./filter/ads-filter/ads-filter.component";
 
 export const routes: Routes = [
   {path: 'ads-list/:id' , component : AdsListComponent},
-  {path: 'learn' , component : LearnComponent}
+  {path: 'learn' , component : LearnComponent},
+  {path: 'adsfilter' , component : AdsFilterComponent}
 ];
